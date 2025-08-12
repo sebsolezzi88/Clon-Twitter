3fd97a057000bb99ecf0721455b14667f3899c66
seba <ssolezzi@gmail.com>
2025-08-12 20:27:33 -0300

    Funcionalidad para actulizar post

--- backend/src/controllers/postController.ts
+++ backend/src/controllers/postController.ts
@@ -26,3 +26,37 @@ export const createPost = async (req: Request, res: Response): Promise<Response>
         return res.status(500).json({ status: 'error', msg: 'Server Error' })
     }
 }
+
+//Actualizar Post
+export const updatePost = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const { text, image } = req.body || {};
+        const {postId} = req.params;
+
+        if (!text || text === '') {
+            return res.status(400).json({ status: 'error', msg: 'Text is requerid' })
+        }
+
+        //Verificar si el post existe
+        const postExist = await Post.findById(postId);
+        
+        if(!postExist){
+            return res.status(404).json({ status: 'error', msg: 'Post not Found' })
+        }
+
+        //Verificar si el post pertence al usuario logueado
+        if(req.userId && postExist.author.toString() !== req.userId.toString()){
+            return res.status(403).json({ status: 'error', msg: 'Forbidden' })
+        }
+        //Actulizar post
+        postExist.text === req.body.text;
+        postExist.text === req.body.image;
+
+        await postExist.save();
+
+        return res.status(201).json({ status: 'success', msg: 'Post updated', post: postExist })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ status: 'error', msg: 'Server Error' })
+    }
+}
--- backend/src/routes/postRoutes.ts
+++ backend/src/routes/postRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createAccount, loginUser } from "../controllers/userController";
 import { validateUserRegister } from "../middlewares/validateRegister";
-import { createPost } from "../controllers/postController";
+import { createPost, updatePost } from "../controllers/postController";
 import { verifyToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post('/',verifyToken,createPost); //Ruta protegida para crear post
+router.put('/:postId',verifyToken,updatePost); //Ruta protegida para actualizar post
+
 
 export default router;
