82b5d6e6fbb8adc6a9494cddb30da7ab0d92f6ec
seba <ssolezzi@gmail.com>
2025-08-12 20:46:08 -0300

    Funcionalidad para obtener los post del usuario logueado

--- backend/src/controllers/postController.ts
+++ backend/src/controllers/postController.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express"
 import { validationResult } from "express-validator";
 import dotenv from 'dotenv';
 import Post from "../models/Post";
-import {isValidObjectId} from "mongoose";
+import { isValidObjectId } from "mongoose";
 
 // Cargar variables de entorno desde .env
 dotenv.config();
@@ -15,7 +15,7 @@ export const createPost = async (req: Request, res: Response): Promise<Response>
         if (!text || text === '') {
             return res.status(400).json({ status: 'error', msg: 'Text is requerid' })
         }
-        
+
         const newPost = await Post.create({
             author: req.userId,
             text,
@@ -32,7 +32,7 @@ export const createPost = async (req: Request, res: Response): Promise<Response>
 export const updatePost = async (req: Request, res: Response): Promise<Response> => {
     try {
         const { text, image } = req.body || {};
-        const {postId} = req.params;
+        const { postId } = req.params;
 
         if (!text || text === '') {
             return res.status(400).json({ status: 'error', msg: 'Text is requerid' })
@@ -40,13 +40,13 @@ export const updatePost = async (req: Request, res: Response): Promise<Response>
 
         //Verificar si el post existe
         const postExist = await Post.findById(postId);
-        
-        if(!postExist){
+
+        if (!postExist) {
             return res.status(404).json({ status: 'error', msg: 'Post not Found' })
         }
 
         //Verificar si el post pertence al usuario logueado
-        if(req.userId && postExist.author.toString() !== req.userId.toString()){
+        if (req.userId && postExist.author.toString() !== req.userId.toString()) {
             return res.status(403).json({ status: 'error', msg: 'Forbidden' })
         }
         //Actulizar post
@@ -66,21 +66,21 @@ export const updatePost = async (req: Request, res: Response): Promise<Response>
 export const deletePost = async (req: Request, res: Response): Promise<Response> => {
     try {
 
-        const {postId} = req.params;
+        const { postId } = req.params;
 
-        if(!postId || !isValidObjectId(postId)){
-             return res.status(400).json({ status: 'error', msg: 'Id requeried' });
+        if (!postId || !isValidObjectId(postId)) {
+            return res.status(400).json({ status: 'error', msg: 'Id requeried' });
         }
 
         //Verificar si el post existe
         const postExist = await Post.findById(postId);
-        
-        if(!postExist){
+
+        if (!postExist) {
             return res.status(404).json({ status: 'error', msg: 'Post not Found' })
         }
-        
+
         //Verificar si el post pertence al usuario logueado
-        if(req.userId && postExist.author.toString() !== req.userId.toString()){
+        if (req.userId && postExist.author.toString() !== req.userId.toString()) {
             return res.status(403).json({ status: 'error', msg: 'Forbidden' })
         }
 
@@ -88,6 +88,27 @@ export const deletePost = async (req: Request, res: Response): Promise<Response>
         await postExist.deleteOne();
 
         return res.status(201).json({ status: 'success', msg: 'Post deleted' })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ status: 'error', msg: 'Server Error' })
+    }
+}
+
+//Obtener los post de un solo usuario por su id
+export const getPostByUserId = async (req: Request, res: Response): Promise<Response> => {
+    try {
+
+        const { userId } = req.params;
+
+        if (!userId || !isValidObjectId(userId)) {
+            return res.status(400).json({ status: 'error', msg: 'Id requeried' });
+        }
+
+        //Buscar los post del usuario
+        const postsExist = await Post.find({ author: userId });
+
+
+        return res.status(201).json({ status: 'success', msg: 'Post deleted', posts: postsExist })
     } catch (error) {
         console.log(error);
         return res.status(500).json({ status: 'error', msg: 'Server Error' })
--- backend/src/routes/postRoutes.ts
+++ backend/src/routes/postRoutes.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
-import { createAccount, loginUser } from "../controllers/userController";
-import { validateUserRegister } from "../middlewares/validateRegister";
-import { createPost, deletePost, updatePost } from "../controllers/postController";
+import { createPost, deletePost, getPostByUserId, updatePost } from "../controllers/postController";
 import { verifyToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post('/',verifyToken,createPost); //Ruta protegida para crear post
+router.get('/:userId',getPostByUserId); //Ruta para obtener los post de un usuario
 router.put('/:postId',verifyToken,updatePost); //Ruta protegida para actualizar post
 router.delete('/:postId',verifyToken,deletePost); //Ruta protegida para borrar post
 
