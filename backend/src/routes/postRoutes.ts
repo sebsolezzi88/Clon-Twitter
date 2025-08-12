86fb4d7ec54968e440205676769e6ebde8d56862
seba <ssolezzi@gmail.com>
2025-08-12 20:38:53 -0300

    Agregada ruta para borrar post

--- backend/src/routes/postRoutes.ts
+++ backend/src/routes/postRoutes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { createAccount, loginUser } from "../controllers/userController";
 import { validateUserRegister } from "../middlewares/validateRegister";
-import { createPost, updatePost } from "../controllers/postController";
+import { createPost, deletePost, updatePost } from "../controllers/postController";
 import { verifyToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post('/',verifyToken,createPost); //Ruta protegida para crear post
 router.put('/:postId',verifyToken,updatePost); //Ruta protegida para actualizar post
+router.delete('/:postId',verifyToken,deletePost); //Ruta protegida para borrar post
+
 
 
 export default router;
