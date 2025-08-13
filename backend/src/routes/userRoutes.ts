b4e018bf5aacd2f4ca0de2a805b435cc760b0e30
seba <ssolezzi@gmail.com>
2025-08-13 10:23:24 -0300

    Agregada rutas para activar cuenta y dejar de seguir usuarios

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
-import { createAccount, followUser, loginUser } from "../controllers/userController";
+import { activateAccount, createAccount, followUser, loginUser, unfollowUser } from "../controllers/userController";
 import { validateUserRegister } from "../middlewares/validateRegister";
 import { verifyToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post('/create',validateUserRegister,createAccount); //Ruta para crear cuenta
+router.get('/activate',activateAccount); //Ruta para activar cuenta
 router.post('/login',loginUser); //Ruta para login y obtener token
 
 //Rutas para seguir usuario
 router.post('/:idUser/follow',verifyToken ,followUser) //Rutas para seguir usuario
+router.post('/:idUser/unfollow',verifyToken ,unfollowUser) //Rutas para dejar de seguir usuario
+
 
 
 
