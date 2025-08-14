553a7f076fbf169cca770521bd944a371d75eb61
seba <ssolezzi@gmail.com>
2025-08-14 09:45:47 -0300

    Agregado ToastContainer al App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,13 @@ import { Register } from './pages/Register';
 import { Login } from './pages/Login';
 import Page404 from './pages/Page404';
 import Main from './pages/Main';
+import { ToastContainer } from 'react-toastify';
 function App() {
 
 
   return (
     <>
+    <ToastContainer/>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Main/>}/>
