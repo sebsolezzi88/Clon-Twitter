6c181eb010f6d0fadb30082715f8ba4e6f7e4c9c
seba <ssolezzi@gmail.com>
2025-08-13 21:47:44 -0300

    Creacion del componente Nabar y agregando a App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
+import Navbar from './components/Navbar';
 
 function App() {
 
 
   return (
     <>
-      <h1>Clon Tuiter</h1>
+      <Navbar/>
     </>
   )
 }
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <header>
+        <h1 className="bg-sky-500">Clon Tuiter</h1>
+        <nav>
+            
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
