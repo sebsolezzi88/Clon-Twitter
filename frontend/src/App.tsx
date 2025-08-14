e36f56cadc798f87911e9eef674e43772543e4a6
seba <ssolezzi@gmail.com>
2025-08-13 21:51:19 -0300

    Agregando rutas de registro y login al app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import Navbar from './components/Navbar';
-
+import { Routes, Route } from 'react-router-dom';
+import { Register } from './pages/Register';
+import { Login } from './pages/Login';
 function App() {
 
 
   return (
     <>
       <Navbar/>
+      <Routes>
+        <Route path='/registro' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
     </>
   )
 }
