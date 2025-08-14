923a9d4c5f97e5d07aae04595facc3488ec67f23
seba <ssolezzi@gmail.com>
2025-08-14 09:43:56 -0300

    Creacion de types para completar formulario

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,6 +1,32 @@
 type Status= 'success' | 'error';
+type Rol= 'user' | 'admin';
+
 
 export interface ApiResponse{
     status: Status;
     msg:string;
 }
+
+//Interface para usuario
+export interface User{
+    username: string;
+    bio?: string;
+    email: string;
+    password: string;
+    role: Rol;
+    isActive: boolean;             
+    activationToken?: string;      
+    createdAt: Date;
+
+    // Relaciones
+    followers?: string[]; 
+    following?: string[]; 
+}
+//Tipo base para el formulario, seleccionando campos existentes
+type UserFormDataBase = Pick<User, 'username' | 'password' | 'email'>;
+//Tipo para el campo adicional (passwordr)
+type PasswordConfirmation = {
+    passwordr: string;
+};
+//Usar el tipo de intersección '&' para combinarlos en un nuevo tipo
+export type UserFormData = UserFormDataBase & PasswordConfirmation;
