e805dc1f942c8e4bb28b0db7aaf6fa5ba8958346
seba <ssolezzi@gmail.com>
2025-08-14 19:36:10 -0300

    Funcion de login para conectarse con la api

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { ApiResponse, RegisterFormData } from "../types/types";
+import type { ApiResponse, LoginApiPostResponse, LoginFormData, RegisterFormData } from "../types/types";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -12,6 +12,22 @@ export const registerUser = async (data:RegisterFormData) =>{
         if (axios.isAxiosError(error)) {
             console.error("Error al registrar usuario:", error.response?.data || error.message);
             throw new Error(error.response?.data?.message || "Error deconocido al registrar");
+        } else {
+            console.error("Error inesperado:", error);
+            throw new Error("Ocurrió un error inesperado.");
+        }
+    }
+}
+
+//Funcion para loguearse 
+export const login = async (data:LoginFormData) =>{
+    try {
+        const res = await axios.post<LoginApiPostResponse>(`${API_URL}/user/login`,data);
+        return res.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error("Error al loguear usuarip:", error.response?.data || error.message);
+            throw new Error(error.response?.data?.message || "Error deconocido al loguearse");
         } else {
             console.error("Error inesperado:", error);
             throw new Error("Ocurrió un error inesperado.");
