95f546ed63c077eb61b8bd480e486e8fd51143c7
seba <ssolezzi@gmail.com>
2025-08-14 19:30:35 -0300

    Agregado disabled al boton

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -69,13 +69,14 @@ export const Login = () => {
           </div>
 
           <button
+          disabled={loading}
             type="submit"
             className="w-full font-black flex justify-center py-2 px-4 border border-transparent 
                            rounded-md shadow-sm text-sm text-white 
                            bg-sky-500 hover:bg-sky-600 
                            transition duration-300"
           >
-            Registrarse
+            Login
           </button>
         </form>
         <div className="text-center text-sm text-gray-600">
