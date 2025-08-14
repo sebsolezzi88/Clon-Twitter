ce8f09254de46447bc51b6704c5df390d2daf16b
seba <ssolezzi@gmail.com>
2025-08-14 19:28:24 -0300

    Creación de estado del loading

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -9,6 +9,9 @@ export const Login = () => {
     password: "",
   });
 
+  //Estado del Loading
+  const [loading, setLoading]=useState<boolean>(false);
+
   return (
     <div className="flex items-center justify-center p-4">
       <div className="max-w-lg w-full bg-white rounded-xl shadow-lg p-8 space-y-6">
