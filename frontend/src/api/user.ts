299ebf5b46ddc98d06f7eb9a48e1fddb177f70f9
seba <ssolezzi@gmail.com>
2025-08-15 10:14:34 -0300

    Funcion para llamar a la api y editar la bio

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,37 +1,87 @@
 import axios from "axios";
-import type { ApiResponse, LoginApiPostResponse, LoginFormData, RegisterFormData } from "../types/types";
-
+import type {
+  ApiResponse,
+  LoginApiPostResponse,
+  LoginFormData,
+  RegisterFormData,
+} from "../types/types";
+import { useAuthStore } from "../storage/authStorage";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 //Funcion para registrar usuarios
-export const registerUser = async (data:RegisterFormData) =>{
-    try {
-        const res = await axios.post<ApiResponse>(`${API_URL}/user/create`,data);
-        return res.data;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            console.error("Error al registrar usuario:", error.response?.data || error.message);
-            throw new Error(error.response?.data?.message || "Error deconocido al registrar");
-        } else {
-            console.error("Error inesperado:", error);
-            throw new Error("Ocurrió un error inesperado.");
-        }
+export const registerUser = async (data: RegisterFormData) => {
+  try {
+    const res = await axios.post<ApiResponse>(`${API_URL}/user/create`, data);
+    return res.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(
+        "Error al registrar usuario:",
+        error.response?.data || error.message
+      );
+      throw new Error(
+        error.response?.data?.message || "Error deconocido al registrar"
+      );
+    } else {
+      console.error("Error inesperado:", error);
+      throw new Error("Ocurrió un error inesperado.");
     }
-}
+  }
+};
 
-//Funcion para loguearse 
-export const loginUser = async (data: LoginFormData):Promise<LoginApiPostResponse> => {
-    try {
-        const res = await axios.post<LoginApiPostResponse>(`${API_URL}/user/login`, data);
-        return res.data;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            // Devuelve el objeto de error completo para que sea manejado por quien llama a la función
-            return error.response?.data;
-        } else {
-            // Lanza un error genérico para errores inesperados
-            throw new Error("Ocurrió un error inesperado.");
-        }
+//Funcion para loguearse
+export const loginUser = async (
+  data: LoginFormData
+): Promise<LoginApiPostResponse> => {
+  try {
+    const res = await axios.post<LoginApiPostResponse>(
+      `${API_URL}/user/login`,
+      data
+    );
+    return res.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      // Devuelve el objeto de error completo para que sea manejado por quien llama a la función
+      return error.response?.data;
+    } else {
+      // Lanza un error genérico para errores inesperados
+      throw new Error("Ocurrió un error inesperado.");
     }
-}
+  }
+};
+
+//Funcion para editar la bio
+export const editBio = async (
+  data: LoginFormData
+): Promise<ApiResponse> => {
+  try {
+    const { user } = useAuthStore();
+    const authToken = user?.token;
+
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${authToken}`,
+    };
+    if (!authToken) {
+      console.error(
+        "No hay token de autenticación disponible. El usuario no está logueado."
+      );
+      throw new Error("No autenticado.");
+    }
+    const res = await axios.post<ApiResponse>(
+      `${API_URL}/user/login`,
+      data,
+      { headers }
+    );
+    return res.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      // Devuelve el objeto de error completo para que sea manejado por quien llama a la función
+      return error.response?.data;
+    } else {
+      // Lanza un error genérico para errores inesperados
+      throw new Error("Ocurrió un error inesperado.");
+    }
+  }
+};
