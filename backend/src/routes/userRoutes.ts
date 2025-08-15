c89627aa9d01431b4df10ac4907d2b60720d8724
seba <ssolezzi@gmail.com>
2025-08-15 10:07:08 -0300

    Agregada ruta para cambiar la bio

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { activateAccount, createAccount, followUser, loginUser, unfollowUser } from "../controllers/userController";
+import { activateAccount, changeBio, createAccount, followUser, loginUser, unfollowUser } from "../controllers/userController";
 import { validateUserRegister } from "../middlewares/validateRegister";
 import { verifyToken } from "../middlewares/authMiddleware";
 
@@ -8,6 +8,9 @@ const router = Router();
 router.post('/create',validateUserRegister,createAccount); //Ruta para crear cuenta
 router.get('/activate',activateAccount); //Ruta para activar cuenta
 router.post('/login',loginUser); //Ruta para login y obtener token
+
+//Ruta editar perfil
+router.patch('/bio',verifyToken,changeBio); //Ruta para editar biografía
 
 //Rutas para seguir usuario
 router.post('/:idUser/follow',verifyToken ,followUser) //Rutas para seguir usuario
