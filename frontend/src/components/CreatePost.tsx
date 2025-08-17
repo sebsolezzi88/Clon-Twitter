a1b3c790f1eb201ea68e497c6e4d762f7c82a600
seba <ssolezzi@gmail.com>
2025-08-17 14:39:54 -0300

    Creacion del componente CreatePost

---
+++ frontend/src/components/CreatePost.tsx
@@ -0,0 +1,27 @@
+const CreatePost = () => {
+  return (
+    <div className="bg-white rounded-xl shadow-lg p-6">
+      <h3 className="text-lg font-bold text-gray-800 mb-4">
+        ¿Qué estás pensando, @NombreDeUsuario?
+      </h3>
+      <form className="space-y-4">
+        <textarea
+          placeholder="Escribe tu nuevo post..."
+          rows={3}
+          className="block w-full px-4 py-2 border border-gray-300 rounded-md 
+                 focus:ring-sky-500 focus:border-sky-500"
+        ></textarea>
+        <button
+          type="submit"
+          className="w-full flex justify-center py-2 px-4 border border-transparent 
+                 rounded-md shadow-sm text-sm font-black text-white bg-sky-500 
+                 hover:bg-sky-600 transition duration-300"
+        >
+          Publicar
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
