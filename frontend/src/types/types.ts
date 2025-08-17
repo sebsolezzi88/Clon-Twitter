a8d25e13b5d56b62278ec9559dd38b7a7badc501
seba <ssolezzi@gmail.com>
2025-08-17 14:58:09 -0300

    Creaccion de interfaz Comment,Post y type PostFormData

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,47 +1,62 @@
-type Status = 'success' | 'error';
-type Rol = 'user' | 'admin';
-
+type Status = "success" | "error";
+type Rol = "user" | "admin";
 
 export interface ApiResponse {
-    status: Status;
-    msg: string;
+  status: Status;
+  msg: string;
 }
 
 //Interface para usuario
 export interface User {
-    username: string;
-    bio?: string;
-    email: string;
-    password: string;
-    role: Rol;
-    isActive: boolean;
-    activationToken?: string;
-    createdAt: Date;
+  username: string;
+  bio?: string;
+  email: string;
+  password: string;
+  role: Rol;
+  isActive: boolean;
+  activationToken?: string;
+  createdAt: Date;
 
-    // Relaciones
-    followers?: string[];
-    following?: string[];
+  // Relaciones
+  followers?: string[];
+  following?: string[];
 }
 //Tipo base para el formulario, seleccionando campos existentes
-type UserFormDataBase = Pick<User, 'username' | 'password' | 'email'>;
+type UserFormDataBase = Pick<User, "username" | "password" | "email">;
 //Tipo para el campo adicional (passwordr)
 type PasswordConfirmation = {
-    passwordr: string;
+  passwordr: string;
 };
 //Usar el tipo de intersección '&' para combinarlos en un nuevo tipo
 export type RegisterFormData = UserFormDataBase & PasswordConfirmation;
 
 //Type loginform
-export type LoginFormData = Pick<User, 'username' | 'password'>
+export type LoginFormData = Pick<User, "username" | "password">;
 
 export interface UserLoginData {
-    username: string;
-    bio?: string;
-    token: string;
+  username: string;
+  bio?: string;
+  token: string;
 }
 
 export interface LoginApiPostResponse extends ApiResponse {
-    userData?:UserLoginData;
+  userData?: UserLoginData;
 }
 //TypeEdit bio
-export type BioFormData = Pick<User, 'bio'>;
+export type BioFormData = Pick<User, "bio">;
+
+//Interfaces para los post
+export interface Comment {
+  user: User;
+  text: string;
+  createdAt: Date;
+}
+export interface Post {
+  author: string;
+  text: string;
+  image?: string;
+  likes?: string[];
+  comments?: Comment[];
+  createdAt: Date;
+}
+export type PostFormData = Pick<Post, "text" | "image">
