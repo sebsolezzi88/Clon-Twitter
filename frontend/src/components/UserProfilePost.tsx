70fe892eed587ba42ed011b39f9fa38dba7909e5
seba <ssolezzi@gmail.com>
2025-08-18 10:14:56 -0300

    Creacion de UserProfilePost

---
+++ frontend/src/components/UserProfilePost.tsx
@@ -0,0 +1,34 @@
+import { ChatBubbleLeftIcon, HeartIcon } from '@heroicons/react/24/outline'
+import React from 'react'
+
+const UserProfilePost = () => {
+  return (
+    <div className="bg-white rounded-xl shadow-lg p-6">
+            <div className="flex items-center space-x-4 mb-4">
+              <div>
+                <span className="font-bold text-gray-800">
+                  @NombreDeUsuario
+                </span>
+                <span className="text-sm text-gray-500 ml-2">
+                  hace 5 minutos
+                </span>
+              </div>
+            </div>
+            <p className="text-gray-700">
+              ¡Hola a todos! Este es mi primer post en mi Clon de Tuiter.
+            </p>
+            <div className="flex items-center space-x-6 mt-4 text-gray-500">
+              <button className="flex items-center space-x-1 hover:text-sky-500 transition-colors">
+                <ChatBubbleLeftIcon className="h-6 w-6 text-gray-500 hover:text-sky-500" />
+                <span>5</span>
+              </button>
+              <button className="flex items-center space-x-1 hover:text-sky-500 transition-colors">
+                <HeartIcon className="h-6 w-6 text-gray-500 hover:text-sky-500" />
+                <span>12</span>
+              </button>
+            </div>
+          </div>
+  )
+}
+
+export default UserProfilePost
