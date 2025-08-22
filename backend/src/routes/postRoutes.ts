3c7da5bab54568a848fbecc739743be14df17f14
seba <ssolezzi@gmail.com>
2025-08-22 10:09:23 -0300

    Añadiendo ruta last10

--- backend/src/routes/postRoutes.ts
+++ backend/src/routes/postRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { commentPost, createPost, deletePost, getPostByUserId, getPostByUserTokenId, updatePost } from "../controllers/postController";
+import { commentPost, createPost, deletePost, getLastTenPost, getPostByUserId, getPostByUserTokenId, updatePost } from "../controllers/postController";
 import { verifyToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
+router.get('/last10',getLastTenPost); //Ruta para obtener los ultimo 10 post si los hay
 router.post('/',verifyToken,createPost); //Ruta protegida para crear post
 router.get('/:userId',getPostByUserId); //Ruta para obtener los post de un usuario
 router.get('/',verifyToken,getPostByUserTokenId); //Ruta para obtener los post de un usuario usando su token
