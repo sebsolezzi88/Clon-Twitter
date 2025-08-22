8e9c9f62e81e980bbda8709f19961ab19106c25c
seba <ssolezzi@gmail.com>
2025-08-22 09:58:40 -0300

    Diseño de Main

--- frontend/src/pages/PageMain.tsx
+++ frontend/src/pages/PageMain.tsx
@@ -2,8 +2,17 @@
 
 const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <div className="px-3">
+      <main className="flex flex-col items-center justify-center p-8 text-white rounded-xl shadow-lg bg-gradient-to-br from-sky-500 to-cyan-500 mx-auto mt-6 max-w-4xl">
+        <h2 className="text-xl md:text-3xl font-bold mb-4 text-center">
+          Únete a la conversación
+        </h2>
+        <p className="text-md md:text-lg text-center max-w-md">
+          Aquí, cada voz importa. Conecta con otros usuarios, comparte tus ideas y descubre lo que el mundo está hablando en este momento.
+        </p>
+      </main>
+    </div>
+  );
+};
 
 export default Main
